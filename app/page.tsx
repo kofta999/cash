4a8135373b52844d86115d852791adac17ea239a
loaai-rashad <rashadloaai@gmail.com>
2024-09-27 05:42:12 +0300

    srarted the sell page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const MyApp = () => {
   return (
     <>
       <Navbar />
-
+ 
     </>
   );
 }
---
+++ app/sell/page.tsx
@@ -0,0 +1,60 @@
+'use client';
+import { title } from 'process';
+import React, {useState} from 'react'
+interface Choice {
+  label: string;
+  value: string;
+}
+
+const Sell = () => {
+  const [selectedChoice, setSelectedChoice] = useState('');
+  const [title, setTitle] = useState ('');
+  const handleChoiceChange = (e) => {
+    setSelectedChoice(e.target.value);
+  };
+
+  const choices = [
+    'Unisex Clothes',
+    'Unisex Accessories',
+    'Women\'s Clothes',
+    'Men\'s Clothing',
+    'Women\'s Accessories',
+    'Men\'s Accessories',
+  ];
+
+  return (
+    
+    <div className="choice-box">
+      <h2>post your Ad</h2>
+
+      {choices.map((choice, index) => (
+        <div key={index} className="choice-item">
+          <input
+            type="radio"
+            id={choice}
+            name="choice"
+            value={choice}
+            checked={selectedChoice === choice}
+            onChange={handleChoiceChange}
+          />
+          <label htmlFor={choice}>{choice}</label>
+        </div>
+      ))}
+
+
+      <div>
+        <label htmlFor="title"> title:</label>
+        <input 
+          type="text"
+          id="input"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder='Enter a descriptive title for your ad'
+        />
+      </div>
+    </div>
+  );
+};
+
+
+export default Sell
